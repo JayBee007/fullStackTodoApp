e4305b5522090dcb450e0ee51d1f92032d866fd7
Jay <jaybee.codes@gmail.com>
2017-09-08 06:09:57 +0000

    add authenticate middleware

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ var _ = require("lodash");
 var {mongoose} = require("./db/mongoose");
 var {Todo} = require("./models/todo");
 var {User} = require("./models/user");
+var {authenticate} = require("./middleware/authenticate");
 
 var app = express();
 
@@ -124,6 +125,12 @@ app.post('/users', (req,res) => {
       res.status(400).send(e);
    });
    
+});
+
+
+
+app.get('/users/me', authenticate, (req,res) => {
+   res.send(req.user);
 });
 
 app.listen(process.env.PORT,process.env.IP,() => {
